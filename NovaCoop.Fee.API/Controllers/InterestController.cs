ef320baba797e754eec057fce1f46515ec5a42e3
Railson Rames <rames.dev@outlook.com>
2021-05-01 23:59:59 -0300

    Add project files.

---
+++ NovaCoop.Fee.API/Controllers/InterestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NovaCoop.Fee.API.Controllers
+{
+    [ApiController]
+    public class InterestController : ControllerBase
+    {
+        [HttpGet("taxaJuros")]
+        public IActionResult GetInterestRate()
+        {
+            return Ok(new decimal(0.01));
+        }
+    }
+}
